e87bb2de1cca293848a14fb610c854c104a563f5
Renato <renatomarquesteles@gmail.com>
2019-08-28 03:20:49 -0300

    chore: Organiza estrutura base

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{justifyContent: 'center', flex: 1}}>
+      <Text style={{fontSize: 36, textAlign: 'center'}}>
+        Hello React Native
+      </Text>
+    </View>
+  );
+};
+
+export default App;
