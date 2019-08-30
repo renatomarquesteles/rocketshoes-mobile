af633409ea002c7e41b3822a2941d046f307b7de
Renato <renatomarquesteles@gmail.com>
2019-08-30 18:22:23 -0300

    feature: ItemCount no header com redux

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import {Image} from 'react-native';
+import {connect} from 'react-redux';
 
 import shoppingBasket from '../../assets/shopping-basket.png';
 import {BasketContainer, Container, ItemCount, Logo, Wrapper} from './styles';
 
-export default function Header({navigation}) {
+function Header({navigation, cartSize}) {
   return (
     <Wrapper>
       <Container>
         <Logo />
         <BasketContainer onPress={() => navigation.navigate('Cart')}>
           <Image style={{width: 24, height: 24}} source={shoppingBasket} />
-          <ItemCount>{0}</ItemCount>
+          <ItemCount>{cartSize || 0}</ItemCount>
         </BasketContainer>
       </Container>
     </Wrapper>
   );
 }
+
+export default connect(
+  state => ({
+    cartSize: state.cart.length,
+  }),
+  null
+)(Header);
