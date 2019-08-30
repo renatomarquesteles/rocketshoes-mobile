c794acec8e98f5004d5b08c1a295015805ee8af4
Renato <renatomarquesteles@gmail.com>
2019-08-30 18:11:40 -0300

    feature: Carrinho

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import {Provider} from 'react-redux';
 import {StatusBar} from 'react-native';
 
 import './config/ReactotronConfig';
 
+import store from './store';
 import Routes from './routes';
+import NavigationService from './services/navigation';
 
 const App = () => {
   return (
-    <>
+    <Provider store={store}>
       <StatusBar barStyle="light-content" backgroundColor="#191920" />
-      <Routes />
-    </>
+      <Routes
+        ref={navigatorRef => NavigationService.setNavigator(navigatorRef)}
+      />
+    </Provider>
   );
 };
 
--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,16 +1,128 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {Image} from 'react-native';
+import * as CartActions from '../../store/modules/cart/actions';
 
-// import { Container } from './styles';
+import {formatPrice} from '../../util/format';
+import deleteForever from '../../assets/delete-forever.png';
+import removeCircleOutline from '../../assets/remove-circle-outline.png';
+import addCircleOutline from '../../assets/add-circle-outline.png';
+import removeShoppingCart from '../../assets/remove-shopping-cart.png';
 
-export default function Cart() {
+import {
+  Container,
+  Products,
+  Product,
+  ProductInfo,
+  ProductImage,
+  ProductDetails,
+  ProductTitle,
+  ProductPrice,
+  ProductDelete,
+  ProductControls,
+  ProductControlButton,
+  ProductAmount,
+  ProductSubtotal,
+  TotalContainer,
+  TotalText,
+  TotalAmount,
+  Order,
+  OrderText,
+  EmptyContainer,
+  EmptyText,
+} from './styles';
+
+function Cart({
+  navigation,
+  products,
+  total,
+  removeFromCart,
+  updateAmountRequest,
+}) {
+  function decrement(product) {
+    updateAmountRequest(product.id, product.amount - 1);
+  }
+
+  function increment(product) {
+    updateAmountRequest(product.id, product.amount + 1);
+  }
+
   return (
-    <View>
-      <Text>Carrinho</Text>
-    </View>
+    <Container>
+      {products.length ? (
+        <>
+          <Products>
+            {products.map(product => (
+              <Product key={product.id}>
+                <ProductInfo>
+                  <ProductImage source={{uri: product.image}} />
+                  <ProductDetails>
+                    <ProductTitle>{product.title}</ProductTitle>
+                    <ProductPrice>{product.priceFormatted}</ProductPrice>
+                  </ProductDetails>
+                  <ProductDelete onPress={() => removeFromCart(product.id)}>
+                    <Image
+                      style={{width: 24, height: 24}}
+                      source={deleteForever}
+                    />
+                  </ProductDelete>
+                </ProductInfo>
+                <ProductControls>
+                  <ProductControlButton onPress={() => decrement(product)}>
+                    <Image
+                      style={{width: 24, height: 24}}
+                      source={removeCircleOutline}
+                    />
+                  </ProductControlButton>
+                  <ProductAmount value={String(product.amount)} />
+                  <ProductControlButton onPress={() => increment(product)}>
+                    <Image
+                      style={{width: 24, height: 24}}
+                      source={addCircleOutline}
+                    />
+                  </ProductControlButton>
+                  <ProductSubtotal>{product.subtotal}</ProductSubtotal>
+                </ProductControls>
+              </Product>
+            ))}
+          </Products>
+          <TotalContainer>
+            <TotalText>TOTAL</TotalText>
+            <TotalAmount>{total}</TotalAmount>
+            <Order>
+              <OrderText>FINALIZAR PEDIDO</OrderText>
+            </Order>
+          </TotalContainer>
+        </>
+      ) : (
+        <EmptyContainer>
+          <Image style={{width: 64, height: 64}} source={removeShoppingCart} />
+          <EmptyText>Seu carrinho está vazio.</EmptyText>
+        </EmptyContainer>
+      )}
+    </Container>
   );
 }
 
-Cart.navigationOptions = {
-  title: 'Cart',
-};
+const mapStateToProps = state => ({
+  products: state.cart.map(product => ({
+    ...product,
+    subtotal: formatPrice(product.price * product.amount),
+    priceFormatted: formatPrice(product.price),
+  })),
+  total: formatPrice(
+    state.cart.reduce(
+      (total, product) => total + product.price * product.amount,
+      0
+    )
+  ),
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(CartActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Cart);
