bd4d5e1b07af9b6d032e6dfe376f55bee48a5e09
Renato <renatomarquesteles@gmail.com>
2019-08-30 11:56:57 -0300

    feature: Listagem na Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,75 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {Component} from 'react';
+import {FlatList} from 'react-native-gesture-handler';
+import {Image} from 'react-native';
 
-// import { Container } from './styles';
+import api from '../../services/api';
+import {formatPrice} from '../../util/format';
+import shoppingBasket from '../../assets/shopping-basket.png';
+import {
+  Background,
+  Container,
+  Product,
+  ProductImage,
+  ProductTitle,
+  ProductPrice,
+  AddButton,
+  ProductAmount,
+  ProductAmountText,
+  AddButtonText,
+} from './styles';
 
-export default function Home({navigation}) {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  );
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  getProducts = async () => {
+    const response = await api.get('/products');
+
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    this.setState({products: data});
+  };
+
+  renderProduct = ({item}) => {
+    return (
+      <Product key={item.id}>
+        <ProductImage source={{uri: item.image}} />
+        <ProductTitle>{item.title}</ProductTitle>
+        <ProductPrice>{item.priceFormatted}</ProductPrice>
+        <AddButton>
+          <ProductAmount>
+            <Image style={{width: 24, height: 24}} source={shoppingBasket} />
+            <ProductAmountText>{0}</ProductAmountText>
+          </ProductAmount>
+          <AddButtonText>ADICIONAR</AddButtonText>
+        </AddButton>
+      </Product>
+    );
+  };
+
+  render() {
+    const {products} = this.state;
+
+    return (
+      <Background>
+        <Container>
+          <FlatList
+            horizontal
+            data={products}
+            extraData={this.props}
+            keyExtractor={item => String(item.id)}
+            renderItem={this.renderProduct}
+          />
+        </Container>
+      </Background>
+    );
+  }
 }
-
-Home.navigationOptions = {
-  title: 'Home',
-};
